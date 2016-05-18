792a09b25dd89357c1b1b8e824c9340f5b7b62a6
Cristobal Vega <cristobalvega96@gmail.com>
2016-05-18 12:07:58 -0600

    Add Activity version 1.1

--- Documents/Tec/4.-Ene_Jun_2016/1.-TAP/Asesorias_Tap/I_lender_u/app/src/main/java/com/example/cristobal/i_lender_u/AddActivity.java
+++ Documents/Tec/4.-Ene_Jun_2016/1.-TAP/Asesorias_Tap/I_lender_u/app/src/main/java/com/example/cristobal/i_lender_u/AddActivity.java
@@ -1,14 +1,28 @@
 package com.example.cristobal.i_lender_u;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddActivity extends AppCompatActivity {
+    Button btnAdd;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        btnAdd=(Button)findViewById(R.id.btnAddActivity);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent MainIntent= new Intent(AddActivity.this,MainActivity.class);
+                startActivity(MainIntent);
+            }
+        });
     }
 }
--- Documents/Tec/4.-Ene_Jun_2016/1.-TAP/Asesorias_Tap/I_lender_u/app/src/main/java/com/example/cristobal/i_lender_u/MainActivity.java
+++ Documents/Tec/4.-Ene_Jun_2016/1.-TAP/Asesorias_Tap/I_lender_u/app/src/main/java/com/example/cristobal/i_lender_u/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.cristobal.i_lender_u;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnAdd;
+    Button btnAddActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAdd=(Button)findViewById(R.id.buttonAdd);
+        btnAddActivity=(Button)findViewById(R.id.buttonAddActivity);
+
+
+        btnAddActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Addintent=new Intent(MainActivity.this,AddActivity.class);
+                startActivity(Addintent);
+            }
+        });
     }
 }
