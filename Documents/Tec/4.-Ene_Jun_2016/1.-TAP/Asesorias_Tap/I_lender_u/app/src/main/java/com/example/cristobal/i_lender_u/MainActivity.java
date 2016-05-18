09a6cfee92d11a603f4d3f7eb65a77ae5b61a214
Cristobal Vega <cristobalvega96@gmail.com>
2016-05-18 11:51:53 -0600

    Add Activity first version

--- Documents/Tec/4.-Ene_Jun_2016/1.-TAP/Asesorias_Tap/I_lender_u/app/src/main/java/com/example/cristobal/i_lender_u/MainActivity.java
+++ Documents/Tec/4.-Ene_Jun_2016/1.-TAP/Asesorias_Tap/I_lender_u/app/src/main/java/com/example/cristobal/i_lender_u/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.cristobal.i_lender_u;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class AddActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add);
+        setContentView(R.layout.activity_main);
+
+        btnAdd=(Button)findViewById(R.id.buttonAdd);
     }
 }
